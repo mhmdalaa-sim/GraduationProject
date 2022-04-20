c06fcb5d36b78bedc945cb002bde08863d96cbc8
mhmdalaa-sim <80651664+mhmdalaa-sim@users.noreply.github.com>
2022-04-20 23:21:55 +0200

    Add files via upload

---
+++ FrontEnd/src/data/TransactionData.jsx
@@ -0,0 +1,46 @@
+const TransactionData =[
+    
+{
+id:"1",
+from:"رئيسي",
+to:"كيمياء",
+quantity:"10",
+unit:"لتر",
+category:"كيماويات",
+name:"كحول ايثيلي",
+price:"500",
+entrydate:"1/1/1",
+exitdate:"2/2/2"
+
+},
+{
+
+id:"2",
+from:"رئيسي",
+to:"كيمياء",
+quantity:"200",
+unit:"لتر",
+category:"كيماويات",
+name:"بيكاربونات صوديوم ",
+price:"400",
+entrydate:"5/1/1",
+exitdate:"7/2/2"
+
+},
+
+{
+
+id:"3",
+from:"رئيسي",
+to:"كيمياء",
+quantity:"200",
+unit:"لتر",
+category:"كيماويات",
+name:" نترات بوتاسيوم ",
+price:"700",
+entrydate:"5/1/1",
+exitdate:"7/2/2"
+}
+
+]
+export default TransactionData
